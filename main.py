7f12264a8f479f7428f0e2f6bd883dc1e4ea281f
AmyMcMahon <admcmahon@live.com>
2024-12-08 12:38:00 +0000

    command line args and logging working

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import argparse
+import sys
+import logging
+from config.config import Config
+from config.logger import Logger
 
 class MainApplication:
     def __init__(self):
-       print("test")
+        self.config = Config("config.json")
+        Logger.setUpLogger(self.config.get('log_path'))
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Main application initialized")
 
     def run(self) -> int:
-        parser = argparse.ArgumentParser(description='Run application in server, client or auto modes') 
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument('-s', action='store_true', help='server mode')
-        group.add_argument('-c', action='store_true', help='client mode')
-        group.add_argument('-a', action='store_true', help='auto mode')
+        try:
+            self.logger.info("Main application running...")
+            parser = argparse.ArgumentParser(description='Run application in server, client or auto modes') 
+            group = parser.add_mutually_exclusive_group(required=True)
+            group.add_argument('-s', action='store_true', help='server mode')
+            group.add_argument('-c', action='store_true', help='client mode')
+            group.add_argument('-a', action='store_true', help='auto mode')
 
-        args = parser.parse_args()
+            args = parser.parse_args()
+        
+        except Exception as e:
+            self.logger.error(f"Error: {e}")
+            return 1
         
 
 def main() -> int:
